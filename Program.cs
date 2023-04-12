c8c837e07de3823d407955b0d5fc69d265d16690
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-12 12:07:20 +0200

    Interface example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Choisir la langue : ");
+Console.WriteLine("1. Français");
+Console.WriteLine("2. Anglais");
+Console.WriteLine("3. Espagnol");
+
+int langue = int.Parse(Console.ReadLine());
+
+IBonjour bonjour = langue switch
+{
+    2 => new BonjourEN(),
+    3 => new BonjourES(),
+    _ => new BonjourFR()
+};
+
+AfficherBonjour(bonjour);
+
+
+
+
+void AfficherBonjour(IBonjour bonjour)
+{
+    Console.WriteLine(bonjour.getBonjour());
+}
+
+public interface IBonjour
+{
+    string getBonjour();
+}
+
+public class BonjourFR : IBonjour
+{
+    public string getBonjour() => "Bonjour";
+}
+
+public class BonjourEN : IBonjour
+{
+    public string getBonjour() => "Hello";
+}
+
+public class BonjourES : IBonjour
+{
+    public string getBonjour() => "Hola";
+}
